f7d91e01d1a894a4e6e33dbd47870dcccb27579e
tufangorel <tufang@gmail.com>
2022-01-16 12:17:10 +0300

    Extend CustomerRepository from QuerydslPredicateExecutor.

--- src/main/java/com/company/customerinfo/repository/CustomerRepository.java
+++ src/main/java/com/company/customerinfo/repository/CustomerRepository.java
@@ -2,11 +2,13 @@ package com.company.customerinfo.repository;
 
 import com.company.customerinfo.model.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
-public interface CustomerRepository extends JpaRepository<Customer, Integer> {
+public interface CustomerRepository extends JpaRepository<Customer, Integer>,
+        QuerydslPredicateExecutor<Customer> {
 
 }
 
