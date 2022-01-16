829d8d525a4f17c9d3a689a2c598325dd8364573
tufangorel <tufang@gmail.com>
2022-01-16 12:19:56 +0300

    Implement integration test for findAllByQueryDSL Spring service layer method.

--- src/test/java/com/company/customerinfo/service/integration/test/CustomerServiceIntegrationTest.java
+++ src/test/java/com/company/customerinfo/service/integration/test/CustomerServiceIntegrationTest.java
@@ -5,12 +5,12 @@ import com.company.customerinfo.CustomerInfoApplication;
 import com.company.customerinfo.model.*;
 import com.company.customerinfo.service.CustomerService;
 import com.company.customerinfo.service.ShippingAddressService;
-import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
+import javax.persistence.EntityManager;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -23,11 +23,16 @@ public class CustomerServiceIntegrationTest {
 
     private final CustomerService customerService;
     private final ShippingAddressService shippingAddressService;
+    private final EntityManager entityManager;
 
     @Autowired
-    public CustomerServiceIntegrationTest(CustomerService customerService, ShippingAddressService shippingAddressService) {
+    public CustomerServiceIntegrationTest(CustomerService customerService,
+        ShippingAddressService shippingAddressService, EntityManager entityManager) {
+
         this.customerService = customerService;
         this.shippingAddressService = shippingAddressService;
+        this.entityManager = entityManager;
+        assertThat(this.entityManager != null);
     }
 
     @Test
@@ -90,4 +95,35 @@ public class CustomerServiceIntegrationTest {
         assertThat( customerFromShippingAddressID != null);
         assertThat( savedRecord == customerFromShippingAddressID );
     }
+
+    @Test
+    public void findAllByQueryDSLTest() {
+
+        Customer customer = new Customer();
+        customer.setName("name1");
+        customer.setAge(1);
+
+        ShippingAddress shippingAddress = new ShippingAddress();
+        Address address = new Address();
+        address.setCountry("TR");
+        address.setCity("Ankara");
+        address.setStreetName("KaleSokak");
+        shippingAddress.setAddress(address);
+        customer.setShippingAddress(shippingAddress);
+
+        CustomerOrder customerOrder = new CustomerOrder();
+        customerOrder.setCustomer(customer);
+        customerOrder.setOrderDate(LocalDateTime.now());
+
+        OrderItem orderItem1 = new OrderItem();
+        orderItem1.setQuantity(1);
+        OrderItem orderItem2 = new OrderItem();
+        orderItem2.setQuantity(2);
+
+        Customer savedRecord = customerService.save(customer);
+
+        List<Customer> customerList =customerService.findAllByQueryDSL();
+        assertThat( customerList.size() > 0);
+    }
+
 }
